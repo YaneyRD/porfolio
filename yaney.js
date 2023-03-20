4b0359329e262fdfeae9a40cd63ccbdc6d375316
YaneyRD <127932783+YaneyRD@users.noreply.github.com>
2023-03-20 15:02:40 +0800

    Delete yaney.js

--- yaney.js
+++
@@ -1,7 +0,0 @@
-var button = document.querySelector("span");
-button.addEventListener("click", () => {
-    button.innerHTML = `<div class="loading-1">Sending</div>`;
-    setTimeout(() => {
-        button.innerText = "Done";
-    }, 5000);
-});
